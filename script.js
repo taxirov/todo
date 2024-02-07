53838c5cbdbadc5090830f9afb9ace3a6deba773
Saad Takhir <saadtakhir1@gmail.com>
2024-02-07 23:50:30 +0500

    2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const newTodoBtn = document.getElementById('newTodoBtn')
-const addNewModal = document.getElementById('addNewModal')
-const closeAddModal = document.getElementById('closeAddModal')
-const createNewTodo = document.getElementById('createNewTodo')
-const doneTodoList = document.getElementById('doneTodoList')
-const undoneTodoList = document.getElementById('undoneTodoList')
-const inputTitle = document.getElementById('inputTitle')
-const inputDesc = document.getElementById('inputDesc')
+const newTodoBtn = document.getElementById('newTodoBtn');
+const addNewModal = document.getElementById('addNewModal');
+const closeAddModal = document.getElementById('closeAddModal');
+const createNewTodo = document.getElementById('createNewTodo');
+const doneTodoList = document.getElementById('doneTodoList');
+const undoneTodoList = document.getElementById('undoneTodoList');
+const inputTitle = document.getElementById('inputTitle');
+const inputDesc = document.getElementById('inputDesc');
 
 let todos = JSON.parse(localStorage.getItem('todos'))
 function getTodos() {
@@ -14,9 +14,11 @@ function getTodos() {
 if (!todos) { localStorage.setItem('todos', JSON.stringify([])) }
 
 async function getLocalTodos() {
-    todos = JSON.parse(localStorage.getItem('todos'))
-    undoneTodoList.innerHTML = ''
-    doneTodoList.innerHTML = ''
+    todos = JSON.parse(localStorage.getItem('todos'));
+    let doneLength = todos.filter(t => t.isDone == true).length;
+    let undoneLength = todos.filter(t => t.isDone == false).length;
+    doneLength == 0 ? doneTodoList.innerHTML = `<p class="text-center text-slate-700 py-8">There are no done todos</p>` : doneTodoList.innerHTML = '';
+    undoneLength == 0 ? undoneTodoList.innerHTML = `<p class="text-center text-slate-700 py-8">There are no undone todos</p>` : undoneTodoList.innerHTML = '';
     if (todos) {
         todos.forEach(todo => {
             if (todo.isDone == false) {
@@ -25,8 +27,8 @@ async function getLocalTodos() {
                                         <p class="text-lg font-bold text-slate-700">${todo.title}</p>
                                         <p class="text-md font-semibold text-slate-700">${todo.desc}</p>
                                         <span class="flex items-center gap-3 w-full">
-                                        <button id="todoDeleteBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 font-bold w-2/5 bg-red-500 rounded-md">Delete</button>
-                                        <button id="todoDoneBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 font-bold w-3/5 bg-green-500 rounded-md">Done</button>
+                                        <button id="todoDeleteBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 hover:shadow-md font-bold w-2/5 bg-red-500 rounded-md">Delete</button>
+                                        <button id="todoDoneBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 hover:shadow-md font-bold w-3/5 bg-green-500 rounded-md">Done</button>
                                         </span>
                                     </li>
                                 `
@@ -36,8 +38,8 @@ async function getLocalTodos() {
                                         <p class="text-lg font-bold text-slate-700">${todo.title}</p>
                                         <p class="text-md font-semibold text-slate-700">${todo.desc}</p>
                                         <span class="flex items-center gap-3 w-full">
-                                            <button id="todoDeleteBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 font-bold w-2/5 bg-red-500 rounded-md">Delete</button>
-                                            <button id="todoUndoneBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-300 font-bold w-3/5 bg-green-200 rounded-md">Undone</button>
+                                            <button id="todoDeleteBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-100 hover:shadow-md font-bold w-2/5 bg-red-500 rounded-md">Delete</button>
+                                            <button id="todoUndoneBtn" todoid="${todo.id}" class="px-4 py-2 text-stone-500 hover:shadow-md font-bold w-3/5 bg-green-200 rounded-md">Undone</button>
                                         </span>
                                     </li>
                                 `
@@ -47,8 +49,7 @@ async function getLocalTodos() {
     }
 }
 
-
-async function checkTodos() {
+function checkTodos() {
     const listItems = document.querySelectorAll('.listItem')
     listItems.forEach(item => {
         item.addEventListener('click', (e) => {
@@ -81,69 +82,46 @@ async function checkTodos() {
             let todos_updated = todos.filter(t => t.id != +button.getAttribute('todoid'))
             localStorage.setItem('todos', JSON.stringify(todos_updated))
             updatePage()
-           } else {
-                console.log(e.target)
-           }
+           } else {}
         })
     })
-    // doneTodoCheck.forEach(i => {
-    //     i.addEventListener('change', (e) => {
-    //         let todo = todos.filter(t => t.id == +e.target.getAttribute('todoid'))[0]
-    //         if (todo.isDone) {
-    //             let todo_updated = {
-    //                 id: todo.id,
-    //                 title: todo.title,
-    //                 desc: todo.desc,
-    //                 isDone: false
-    //             }
-    //             console.log(todo_updated)
-    //         } else {
-    //             let todo_updated = {
-    //                 id: todo.id,
-    //                 title: todo.title,
-    //                 desc: todo.desc,
-    //                 isDone: true
-    //             }
-    //             console.log(todo_updated)
-    //         }
-    //         localStorage.setItem('todos', JSON.stringify(todos))
-    //         updatePage()
-    //     })
-    // })
-}
-async function updatePage() {
-    await getLocalTodos()
-    await checkTodos()
 }
 
-updatePage().then(() => {
-
-    newTodoBtn.addEventListener('click', () => {
-        addNewModal.classList.remove('hidden')
-        addNewModal.classList.add('flex')
+function updatePage() {
+    getLocalTodos().then(() => {
+        checkTodos()
+    }).catch((e) => {
+        console.log(e)
     })
+}
 
-    function CloseAddModal() {
-        addNewModal.classList.remove('flex')
-        addNewModal.classList.add('hidden')
+function CloseAddModal() {
+    addNewModal.classList.remove('flex')
+    addNewModal.classList.add('hidden')
+}
+
+updatePage()
+
+newTodoBtn.addEventListener('click', () => {
+    addNewModal.classList.remove('hidden')
+    addNewModal.classList.add('flex')
+})
+
+closeAddModal.addEventListener('click', () => {
+    CloseAddModal()
+})
+
+createNewTodo.addEventListener('click', () => {
+    let newTodo = {
+        id: todos == null ? 0 : todos.length,
+        title: inputTitle.value,
+        desc: inputDesc.value,
+        isDone: false
     }
-
-    closeAddModal.addEventListener('click', () => {
-        CloseAddModal()
-    })
-
-    createNewTodo.addEventListener('click', () => {
-        let newTodo = {
-            id: todos == null ? 0 : todos.length,
-            title: inputTitle.value,
-            desc: inputDesc.value,
-            isDone: false
-        }
-        todos.push(newTodo)
-        localStorage.setItem('todos', JSON.stringify(todos))
-        updatePage()
-        CloseAddModal()
-    })
+    todos.push(newTodo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    updatePage()
+    CloseAddModal()
 })
 
 
